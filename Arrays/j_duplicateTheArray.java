0aabf7bf3ba5bdc41b2fb8286e736e548f7e1b41
Aman Mishra <mishraaman113114@gmail.com>
2022-07-02 23:58:26 +0530

    duplicates the array

--- Arrays/j_duplicateTheArray.java
+++ Arrays/j_duplicateTheArray.java
@@ -23,23 +23,46 @@ public class j_duplicateTheArray {
     }
 
     public static ArrayList<Integer> duplicates(int arr[], int n){
-        ArrayList<Integer> ans = new ArrayList<>();
-            for(int i = 0; i < arr.length; i++){
-                for(int j = i+1; j < arr.length; j++){
-                    if(arr[i] == arr[j]){
-                        if(i == j || ans.contains(arr[i])){
-                            continue;
-                        }else{
-                            ans.add(arr[i]);
-                        }
-                    }
-                }
-            }
-            if(ans.isEmpty()){
-                ans.add(-1);
-            }
-            Collections.sort(ans);
-            return ans;
+        // ArrayList<Integer> ans = new ArrayList<>();
+        //     for(int i = 0; i < arr.length; i++){
+        //         for(int j = i+1; j < arr.length; j++){
+        //             if(arr[i] == arr[j]){
+        //                 if(i == j || ans.contains(arr[i])){
+        //                     continue;
+        //                 }else{
+        //                     ans.add(arr[i]);
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     if(ans.isEmpty()){
+        //         ans.add(-1);
+        //     }
+        //     Collections.sort(ans);
+        //     return ans;
+    
+        // ANOTHER APPROCH
+        ArrayList<Integer> l =  new ArrayList<>();
+       
+        for(int i = 0;i<n ;i++){
+         
+         arr[arr[i]%n] +=n;
+            
+        }
+        
+        for(int i = 0;i<n ;i++){
+         
+         if( arr[i] / n >1 ){
+             l.add(i);
+         }
+            
+        }
+       
+        if(l.size() == 0){
+            l.add(-1);
+        }
+        
+        return l;
     }
 
 }
