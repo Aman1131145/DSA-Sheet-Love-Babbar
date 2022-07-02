6df4dccd8d984ebe0dfca6b8f0d33cc3ba601b72
Aman Mishra <mishraaman113114@gmail.com>
2022-07-02 20:25:14 +0530

    Sort the array

---
+++ Arrays/d_Sort.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.*;
+
+public class d_Sort {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+        sort012(a, n);
+        for(int i: a){
+            System.out.print(i + " ");
+        }
+        sc.close();
+    }
+    public static void sort012(int a[], int n)
+    {
+        Arrays.sort(a);
+    }
+}
